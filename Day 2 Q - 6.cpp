446f8d89dd1e2873ee05e3ef82554ea9f9a29557
Manobala.R <manoravi005@gmail.com>
2025-11-02 03:21:37 -0800

    Create Day 2 Q - 6.cpp

---
+++ Day 2 Q - 6.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int m1, m2, m3, m4, m5;
+
+    printf("Enter marks in 5 subjects: ");
+    scanf("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+
+    printf("Marks in Subject 1: %d\n", m1);
+    printf("Marks in Subject 2: %d\n", m2);
+    printf("Marks in Subject 3: %d\n", m3);
+    printf("Marks in Subject 4: %d\n", m4);
+    printf("Marks in Subject 5: %d\n", m5);
+
+    return 0;
+}
